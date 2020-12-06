b33705e2bc7d949390bdc34b4c53558b90b53f4b
waleedd32 <valid0@hotmail.com>
2020-12-06 19:44:12 +0200

    Designing process

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>This is Going to become a Google Clone !</h1>
+      {/* Homepage (the one the search on ) */}
+      <Home />
+      {/* SearchPage (page where is results) */}
     </div>
   );
 }
