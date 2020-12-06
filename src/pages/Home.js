98bc551689070f4672a1d6150cea5473919bbc10
waleedd32 <valid0@hotmail.com>
2020-12-06 20:09:55 +0200

    added router Link's to Home.js headers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div className="home">
       <h1>This is Home Page</h1>
       <div className="home__header">
         <div className="header__headerLeft">
-          {/* Link */}
-          {/* Link */}
-          {/* Link */}
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
         <div className="header__headerRight">
-          {/* Link */}
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+
           {/* Link */}
           {/* Icon */}
           {/* Avatar */}
