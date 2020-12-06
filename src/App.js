090ca73f45e246cf02fb61b1a98b994951576414
waleedd32 <valid0@hotmail.com>
2020-12-06 19:55:07 +0200

    installed material-ui using npm and more designing in Home.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
+    //BEM naming
     <div className="app">
       <h1>This is Going to become a Google Clone !</h1>
       {/* Homepage (the one the search on ) */}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
-import React from "react";
-
 function Home() {
   return (
     <div className="home">
       <h1>This is Home Page</h1>
+      <div className="home__header">
+        <div className="header__headerLeft">
+          {/* Link */}
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className="header__headerRight">
+          {/* Link */}
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+      <div className="home__body"></div>
     </div>
   );
 }
