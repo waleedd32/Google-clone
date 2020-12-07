9e3d5aaa2e53f54211330dabbc86f764163ffdc0
waleedd32 <valid0@hotmail.com>
2020-12-07 21:31:23 +0200

    created Search component where is for now added input and material-ui icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import { Avatar } from "@material-ui/core";
 import AppsIcon from "@material-ui/icons/Apps";
+import Search from "../components/Search";
 
 function Home() {
   return (
@@ -24,6 +25,9 @@ function Home() {
           src="https://www.google.co.in/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
           alt=""
         />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
       </div>
     </div>
   );
