9539d6c08c5f4dd337ef7d6a2a59aa2585090d6f
waleedd32 <valid0@hotmail.com>
2020-12-10 20:55:10 +0200

    added buttons(material-ui) for our google search

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,7 @@ import "../components/Search";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import "./Search.css";
+import { Button } from "@material-ui/core";
 
 function Search() {
   return (
@@ -11,6 +12,10 @@ function Search() {
         <SearchIcon className="search__inputIcon" />
         <input />
         <MicIcon />
+      </div>
+      <div className="search__buttons">
+        <Button variant="outlined">Google Search</Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
   );
