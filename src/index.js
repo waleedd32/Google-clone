4ca2cdf5d27036005ea6a471be218119b3451763
waleedd32 <valid0@hotmail.com>
2020-12-17 00:39:18 +0200

    created StateProvider.js where 'children' is referred to 'App'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { StateProvider } from "./datalayer/StateProvider";
 
 ReactDOM.render(
   <React.StrictMode>
