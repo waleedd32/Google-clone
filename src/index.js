9845d60fb44bdbd950d5b49ab254b6642faea912
waleedd32 <valid0@hotmail.com>
2020-12-18 19:25:26 +0200

    created reducer.js which has also initialState

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { StateProvider } from "./datalayer/StateProvider";
+import reducer, { initialState } from "./datalayer/reducer";
 
 ReactDOM.render(
   <React.StrictMode>
