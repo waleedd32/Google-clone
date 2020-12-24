d969da87c690f92fac972358470e2c85956e589c
waleedd32 <valid0@hotmail.com>
2020-12-24 02:07:36 +0200

    got key from google developers site and created keys.js

---
+++ src/keys.js
@@ -0,0 +1,6 @@
+export const API_KEY = "YOUR_API_KEY";
+
+export default API_KEY;
+
+// Your can get the key from here :
+// https://developers.google.com/custom-search/v1/introduction#identify_your_application_to_google_with_api_key
