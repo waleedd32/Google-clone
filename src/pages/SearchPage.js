d852a4677475f85ea229d5b83c529b3cb82dc96b
waleedd32 <valid0@hotmail.com>
2021-01-07 23:33:19 +0200

    created search page options

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -5,6 +5,13 @@ import useGoogleSearch from "../useGoogleSearch";
 import "./SearchPage.css";
 import { Link } from "react-router-dom";
 import Search from "../components/Search";
+import SearchIcon from "@material-ui/icons/Search";
+import Description from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 function SearchPage() {
   const [{ term = "michael jackson" }, dispatch] = useStateValue();
   // Live API CALL
@@ -25,6 +32,42 @@ function SearchPage() {
         </Link>
         <div className="searchPage__headerBody">
           <Search hideButtons />
+          <div className="searchPage__options">
+            <div className="searchPage__optionsLeft">
+              <div className="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage__option">
+                <Description />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage__option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage__option">
+                <LocalOfferIcon />
+                <Link to="/shopping">Shopping</Link>
+              </div>
+              <div className="searchPage__option">
+                <RoomIcon />
+                <Link to="/maps">Maps</Link>
+              </div>
+              <div className="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/more">More</Link>
+              </div>
+            </div>
+            <div className="searchPage__optionsRight">
+              <div className="searchPage__option">
+                <Link to="/settings">Settings</Link>
+              </div>
+              <div className="searchPage__option">
+                <Link to="/tools">Tools</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
